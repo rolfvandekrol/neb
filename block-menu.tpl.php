bcafa184f293a0d4432a953c50c6777f89dc55c5
mortendk <drupal@morten.dk>
2009-04-16 11:16:51 +0000

    removed blocvk-menu

--- block-menu.tpl.php
+++
@@ -1,9 +0,0 @@
-<div id="block-<?php print $block->module . '-' . $block->delta; ?>" class="<?php print $classes; ?>">
-  <div class="block-inner">
-
-  <?php print $block->content; ?>
-
-  <?php print $edit_links; ?>
-
-  </div>
-</div>
