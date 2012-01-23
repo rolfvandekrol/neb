cd8c4e974be59b3207507491a00079c3d91e0db3
mortendk <morten@geekroyale.com>
2012-01-23 11:03:22 +0100

    user account page titles
    Signed-off-by: mortendk <morten@geekroyale.com>

--- tema/template.php
+++ tema/template.php
@@ -1,8 +1,6 @@
 <?php
 
 function tema_preprocess_html(&$vars, $hook) {
-  //get some fancy fonts
-
   //Test to see if we have the modules we needs,
   if(!module_exists('blockify')){
    // drupal_set_message('Tema use the blockify module - so you can move the logo, title, taps where you wants to & makes the page.tpl easier to work with: <a href="http://drupal.org/project/blockify">Download</a>', 'error');
@@ -10,6 +8,25 @@ function tema_preprocess_html(&$vars, $hook) {
   }else{
     //needs to add messages to the page cause we aint
   }
-
-
 }
+
+/*
+add classes to forms
+*/
+function tema_form_alter(&$form, &$form_state, $form_id) {
+//  print $form_id;
+  if( $form_id == "user_login"){
+    $form['#attributes']['class'] = "tooltip";
+  }elseif($form_id == "user_register_form"){
+    $form['#attributes']['class'] = "tooltip";
+  }elseif($form_id == "user_pass"){
+    $form['#attributes']['class'] = "tooltip";
+  }elseif($form_id == "user_login_block"){
+  //  $form['#attributes']['class'] = "tooltip";    
+  }
+}
+
+
+
+
+
