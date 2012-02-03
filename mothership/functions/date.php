442f924299d274d7743b3c1690c1a2f71f748e92
mortendk <morten@geekroyale.com>
2012-02-03 12:09:28 +0100

    removed the classes from a date - we have time n stuff for it now
    Signed-off-by: mortendk <morten@geekroyale.com>

--- mothership/functions/date.php
+++ mothership/functions/date.php
@@ -2,12 +2,12 @@
 function mothership_date_display_single($vars) {
   $date = $vars['date'];
   $timezone = $vars['timezone'];
-  $vars['attributes']['datetime'] = $vars['dates']['value']['formatted_iso']; 
 
+  $vars['attributes']['datetime'] = $vars['dates']['value']['formatted_iso']; 
   $attributes = $vars['attributes'];
  
-  // Wrap the result with the attributes.
-//  return '<span class="date-display-single"' . drupal_attributes($attributes) . '>' . $date . $timezone . '</span>';
+// Wrap the result with the attributes.
+// used to be return '<span class="date-display-single"' . drupal_attributes($attributes) . '>' . $date . $timezone . '</span>';
   return '<time ' . drupal_attributes($attributes) . '>' . $date . $timezone . '</time>';
 
 }
