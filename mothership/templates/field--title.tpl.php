6e08dccaf1b9122f2a28afaedb5f9760298f32bb
mortendk <morten@geekroyale.com>
2012-02-14 02:31:47 +0100

    field title tpl - removing all wrappers around a title
    yup can't seem to find a good reason why we would wrap an title in anything than the h1-h6

---
+++ mothership/templates/field--title.tpl.php
@@ -0,0 +1,13 @@
+<!-- field--title.tpl -->
+  <?php if(count($items) > 1 ){ ?>
+    <div class="field-items"<?php print $content_attributes; ?>>
+      <?php foreach ($items as $delta => $item) : ?>
+        <div class="field-item <?php print $delta % 2 ? 'odd' : 'even'; ?>"<?php print $item_attributes[$delta]; ?>><?php print render($item); ?></div>
+      <?php endforeach; ?>
+    </div>
+  <?php }else{ ?>
+      <?php foreach ($items as $delta => $item) : ?>
+        <?php print render($item); ?>
+      <?php endforeach; ?>
+  <?php } ?>
+
